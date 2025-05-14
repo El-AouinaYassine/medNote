c3ec4ecd9a309f1166ecdd3be279804e10c113e6
El-AouinaYassine <yassine.elaouina1@usmba.ac.ma>
2025-05-14 19:53:47 +0100

    solved api routes problem

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,5 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use app\Http\Controllers\PatientController;
+use App\Models\Patient;
 
-Route::apiResource('posts', PostController::class);
+Route::get('/patients', function () {
+    return Patient::all();
+});
+
+Route::get('/patients/{id}', function ($id) {
+    $patient = Patient::find($id);
+
+    if (!$patient) {
+        return response()->json(['message' => 'Patient not found'], 404);
+    }
+
+    return $patient;
+});
